ae14e7dde5825e070c51e59d91d5964e579d37bd
Franco <belluccifranco@gmail.com>
2013-10-10 01:42:36 -0300

    Nueva cuenta de mega para test

--- src/main/java/com/vinilo/controller/ReproductorController.java
+++ src/main/java/com/vinilo/controller/ReproductorController.java
@@ -1,7 +1,7 @@
 package com.vinilo.controller;
 
 import com.vinilo.model.Cancion;
-import com.vinilo.service.ReproductorService;
+import com.vinilo.service.CancionService;
 import com.vinilo.service.mega.MegaHandler;
 import java.io.IOException;
 import java.util.List;
@@ -14,18 +14,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ReproductorController {
 
-    private MegaHandler mh = new MegaHandler("belluccifranco@gmail.com", "mega01");
-    private ReproductorService reproductorService;
+    private MegaHandler mh = new MegaHandler("cuentavinilo01@gmail.com", "Ninguna01");
+    private CancionService CancionService;
 
     @Autowired
-    public ReproductorController(ReproductorService reproductorService) {
-        this.reproductorService = reproductorService;
+    public ReproductorController(CancionService cancionService) {
+        this.CancionService = cancionService;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String show() {
         try {
-            List<Cancion> canciones = reproductorService.buscarTodas();
+            //List<Cancion> canciones = CancionService.buscarTodas();
             mh.login();
         } catch (IOException ex) {
             System.out.println(ex);
@@ -36,13 +36,10 @@ public class ReproductorController {
     @RequestMapping(value = "/play", method = RequestMethod.GET)
     public void playMusic(HttpServletResponse response) {
         try {
-            //"https://mega.co.nz/#!5VRhBA4C!EOoONX5nCjzhkqdsmLkX0XkKbuxYALsCvvrkX8psoR0"
-            //"https://mega.co.nz/#!AUIRxSqS!FYZsZEPIISDuqz6eDTDTGD_WNBBH9SHnu8yCVnXiykk"
-
             response.addHeader("Connection", "Keep-Alive");
             response.addHeader("Keep-Alive", "timeout=600"); //600 seg = 10 min
             response.setContentType("audio/mpeg");
-            String urlMega = "https://mega.co.nz/#!AUIRxSqS!FYZsZEPIISDuqz6eDTDTGD_WNBBH9SHnu8yCVnXiykk";
+            String urlMega = "https://mega.co.nz/#!u05HDKSC!DauTn0lUVaUOjauoxXXqpk5Y4qYi3wVCA46XLZbontk";
             mh.streamToOutputStream(urlMega, response);
         } catch (Exception ex) {
             System.out.println(ex);
