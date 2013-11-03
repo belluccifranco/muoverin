78fad2f64ed3750156733febf453dd59c2f2d88a
Franco <belluccifranco@gmail.com>
2013-11-03 15:32:16 -0300

    Ordenando un poco el service para la paginacion

--- src/main/java/com/vinilo/controller/BuscadorController.java
+++ src/main/java/com/vinilo/controller/BuscadorController.java
@@ -5,7 +5,6 @@ import com.vinilo.service.CancionService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,8 +29,8 @@ public class BuscadorController {
     @RequestMapping(value = "/canciones", method = RequestMethod.GET, params = {"criteria", "indice"})
     @ResponseBody
     public List<Cancion> buscarConCriteria(@RequestParam(value = "criteria") String criteria,
-            @RequestParam(value = "indice", defaultValue = "1") int indice) {
+            @RequestParam(value = "indice", defaultValue = "0") int indice) {
 
-        return cancionService.buscarConCriteria(criteria, indice);
+        return cancionService.buscarPorNombreCancionNombreArtistaNormbreAlbum(criteria, indice);
     }
 }
