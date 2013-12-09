13db8e4f6ee2e83549011b5a00aed34d0f72da9b
Franco <belluccifranco@gmail.com>
2013-12-09 09:59:02 -0300

    Primeros agregados de seguridad

--- src/main/java/com/vinilo/controller/CuentaUsuarioController.java
+++ src/main/java/com/vinilo/controller/CuentaUsuarioController.java
@@ -1,5 +1,21 @@
 package com.vinilo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class CuentaUsuarioController {
-    
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(ModelMap model) {
+        return "login";
+    }
+
+    @RequestMapping(value = "/loginFailed", method = RequestMethod.GET)
+    public String loginError(ModelMap model) {
+        model.addAttribute("error", "true");
+        return "login";
+    }
 }
